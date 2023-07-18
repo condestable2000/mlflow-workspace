4b65871749689e401d5e5fb6bc18a37a26a3f38c
condestable2000 <condestable2000@users.noreply.github.com>
2023-07-18 21:02:49 +0200

    Update generate_keys.sh
    Añadidos la eliminación previa de los archivos de keys y la passphrase en blanco para evitar que pare el script

--- keys/generate_keys.sh
+++ keys/generate_keys.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-ssh-keygen -t ed25519 -f ssh_host_ed25519_key < /dev/null
-ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key < /dev/null
+rm *key *.pub
+ssh-keygen -t ed25519 -f ssh_host_ed25519_key -N "" < /dev/null
+ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key -N "" < /dev/null
